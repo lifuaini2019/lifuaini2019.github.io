2aa32f6e7890d28addf3091ffaa8fa22725d179a
Your Name <lifuaini2019@gmail.com>
2023-09-25 17:56:40 +0800

    Site updated: 2023-09-25 17:56:40

--- js/custom.js
+++ js/custom.js
@@ -4,11 +4,3 @@
 
 
 })(jQuery);
-
-// 添加复制按钮
-var copyButton = document.createElement('button');
-copyButton.className = 'copy-button'; // 添加样式类，用于样式设置
-copyButton.textContent = '一键复制代码'; // 设置按钮文本为中文
-
-// 将按钮添加到代码块后面
-codeBlock.parentNode.insertBefore(copyButton, codeBlock.nextSibling);
